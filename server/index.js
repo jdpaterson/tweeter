4e024163d5ad81ecc7df6afaf4263300e73a94a5
Jeremy Paterson <jaydpaterson@gmail.com>
2018-06-22 19:30:21 +0000

    fixing heroku bugs

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 "use strict";
 
 // Basic express setup:
+require('dotenv').config();
 
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-let db;
+const MONGODB_URI = process.env.MONGODB_URI;//"mongodb://localhost:27017/tweeter";
+
 // const mongo = require('./mongo').myConnect;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,12 +19,12 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   console.log('Connected to MongoDB');
   const DataHelpers = require("./lib/data-helpers.js")(db);
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-  // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
-
   app.listen(PORT, () => {
-    console.log("Listening on port " + PORT);
-
+    console.log('Logged into web server');
   });
+
+
+
+
 })
