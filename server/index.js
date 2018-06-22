6e8855a3c2c122738103eb7664f6b46b8dc88cd1
Jeremy Paterson <jaydpaterson@gmail.com>
2018-06-22 19:36:49 +0000

    fixing heroku port issue

--- server/index.js
+++ server/index.js
@@ -9,19 +9,19 @@ const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = process.env.MONGODB_URI;//"mongodb://localhost:27017/tweeter";
-
+console.log('MongoDB URI', MONGODB_URI);
 // const mongo = require('./mongo').myConnect;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
-  console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB at: ', MONGODB_URI);
   const DataHelpers = require("./lib/data-helpers.js")(db);
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
   app.use("/tweets", tweetsRoutes);
-  app.listen(PORT, () => {
-    console.log('Logged into web server');
+  app.listen(process.env.PORT, () => {
+    console.log('Logged into web server at port: ', PORT);
   });
 
 
